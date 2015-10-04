373b2ffe4562bc87c6b926409781d388fb199aeb
YoonHo-Lee <xpzkaos_@naver.com>
2015-10-04 20:21:23 +0900

    layout

--- app/src/main/java/com/example/raven/calculator/MainActivity.java
+++ app/src/main/java/com/example/raven/calculator/MainActivity.java
@@ -1,19 +1,111 @@
 package com.example.raven.calculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView tv_result;
+    TextView tv_sum, tv_op1, tv_op2;
+
+    String  operand1, operand2, sum;
+    int  result;
+
+    boolean m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        tv_sum = (TextView) findViewById(R.id.tv_sum);
+        tv_op1 = (TextView) findViewById(R.id.tv_op1);
+        tv_op2 = (TextView) findViewById(R.id.tv_op2);
+
+        m = true;
+        sum="";
+        operand1 = "";
+        operand2 = "";
+
+
     }
 
+    public void onNumberClick(View view)    {
+        Button btn = (Button)view;
+
+        tv_result.append(btn.getText().toString());
+
+    }
+
+    public void onOperatorClick(View view)  {
+        Button btn = (Button)view;
+
+        if(btn.getText().equals("C"))   {
+            tv_sum.setText(btn.getText().toString());
+            sum="";
+            operand1="";
+            operand2="";
+            tv_result.setText("0");
+            m=true;
+
+            showTest();
+
+            return;
+        }
+
+        if(btn.getText().equals("="))   {
+            tv_sum.setText(btn.getText().toString());
+
+            sum = tv_result.getText().toString();
+/*            result = Integer.parseInt(sum);
+
+            tv_result.setText(""+result);*/
+
+
+            return;
+        }
+
+        if(btn.getText().equals("+"))   {
+            tv_result.append(btn.getText().toString());
+
+
+
+            showTest();
+
+
+            return;
+        }
+
+        if(btn.getText().equals("-"))   {
+            tv_sum.setText(btn.getText().toString());
+
+            return;
+        }
+
+        if(btn.getText().equals("*"))   {
+            tv_sum.setText(btn.getText().toString());
+
+            return;
+        }
+
+
+    }
+
+    public void toInteger() {
+    }
+
+    public void showTest()  {
+        tv_op1.setText(operand1);
+        tv_op2.setText(operand2);
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
